0f73494ded1ac7ca88781d4a009212e4117ad0fe
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-01-17 12:22:09 +0300

    PAY-71 testing sentry integration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /**
  * App entry point
  */
+import * as Sentry from '@sentry/browser';
 import P1PayOne from './P1PayOne';
 
 // Public API
@@ -8,3 +9,12 @@ if (typeof window !== 'undefined') {
   window.P1PayOne = P1PayOne;
 }
 export default P1PayOne;
+
+Sentry.init({
+  dsn: 'https://3e4a24900f064243a9de45162660a66d@sentry.tst.protocol.one/3',
+  // ...
+});
+
+// Raven.context(function () {
+//   initMyApp();
+// });
