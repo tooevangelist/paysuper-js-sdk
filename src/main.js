890e37f4b1f790086c2cbdd0ac68187bf12f656f
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-01-18 12:23:48 +0300

    PAY-71 removes sentry integration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 /**
  * App entry point
  */
-import Raven from 'raven-js';
 import P1PayOne from './P1PayOne';
 
 // Public API
@@ -9,11 +8,3 @@ if (typeof window !== 'undefined') {
   window.P1PayOne = P1PayOne;
 }
 export default P1PayOne;
-
-Raven.config('https://3e4a24900f064243a9de45162660a66d@sentry.tst.protocol.one/3').install();
-// Raven.context(function () {
-//   initMyApp();
-// });
-setTimeout(() => {
-  Raven.captureException('TEST');
-}, 2000);
